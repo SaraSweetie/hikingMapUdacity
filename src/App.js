a792723e76734a97f12a42d124d3d376fe041e3f
Sara Krum <sarasmith887@gmail.com>
2018-10-08 21:51:33 -0400

    map loaded

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount () {
+    window.initMap= this.initMap()
+  }
+
+  initMap = () => {
+        let map = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>title</h1>
         </header>  
-        <body>
+        <main>
           <div id="map"></div>
-
-        </body>
+        </main>
       </div>
     );
   }
 }
+
 
 export default App;
