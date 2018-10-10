d7ecc7d5aacca779bdf51a91877387cd567928c1
Sara Krum <sarasmith887@gmail.com>
2018-10-09 20:47:22 -0400

    map rendered properly

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 
 class App extends Component {
   componentDidMount () {
-    window.initMap= this.initMap()
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCuiW5creiml57NOunZtpggZ59_XjAl5FI&callback=initMap")
+    window.initMap = this.initMap
   }
 
   initMap = () => {
-        let map = new window.google.maps.Map(document.getElementById('map'), {
+        var map = new window.google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
@@ -27,5 +32,14 @@ class App extends Component {
   }
 }
 
+
+  function loadScript(src) {
+    var index = window.document.getElementsByTagName('script')[0]
+    var script = window.document.createElement('script')
+    script.src = src
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+  }
 
 export default App;
